e73ccf5a8836945dfa5ab04eb5439de3a5adc8ef
shifa0722 <pshifa304@gmail.com>
2026-09-24 23:48:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+import Products from './assets/pages/Products'
+import Register from './assets/pages/Register'
+import Login from './assets/pages/Login.Jsx'
+
+function App() {
+  
+
+  return (
+    < Login/>
+ 
+  );
+}
+
+export default App
